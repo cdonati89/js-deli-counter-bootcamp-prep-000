b67713d667cd7323475ec70fe29a7f41a7cdf50b
Christopher Donati <cdonati89@gmail.com>
2017-11-02 20:14:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,15 @@ function nowServing(katzDeli) {
     return(`Currently serving ${name}.`);
   }
 }
+function currentLine(katzDeli) {
+  var line = []
+  if (katzDeli.length === 0) {
+    return "The line is currently empty."  
+  }
+  else {
+    for (let i = 0; i < katzDeli.length, i++) {
+      line += `${i + 1}. ${katzDeli[i]}, `
+    }
+    return `The line is currently: ${line}`;    
+  }
+}
