b0bde731275750f3509758da8329ad758ba2c7df
Christopher Donati <cdonati89@gmail.com>
2017-11-02 19:24:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var katzDeli = [];
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   var position = katzDeliLine.indexOf(name) + 1;
-  return position;
+  return `Welcome ${name}. You are number ${position} in line.`;
 }
+
