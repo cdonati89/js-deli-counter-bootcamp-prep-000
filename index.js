36f8f129b63bbdd05e489ff70f577612158e604c
Christopher Donati <cdonati89@gmail.com>
2017-11-02 19:41:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function nowServing() {
   if (katzDeli.length > 0) {
     var name = katzDeli[0];
     katzDeli.shift();
-    return `Now serving ${name}.`;
+    return `Currently serving ${name}.`;
       }
   else {
     return "There is nobody waiting to be served!"
