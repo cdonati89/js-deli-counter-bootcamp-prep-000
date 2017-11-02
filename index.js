304739617a7785494a8a7c8ba59d955e78b953ae
Christopher Donati <cdonati89@gmail.com>
2017-11-02 19:28:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-var katzDeli = [];
+var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   var position = katzDeliLine.indexOf(name) + 1;
   return `Welcome, ${name}. You are number ${position} in line.`;
+}
+
+function nowService() {
+  var position = katzDeliLine.indexOf(name) + 1;
+  if (katzDeliLine.length > 0) {
+    return `Now serving number ${position}`
+  }
 }
